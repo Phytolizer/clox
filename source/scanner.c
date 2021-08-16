35d267774a0a0fdae95d62e3d4c55daae024633e
Phytolizer <kyle.coffey@tamu.edu>
2021-08-15 19:49:26 -0500

    keywords/identifiers

--- source/scanner.c
+++ source/scanner.c
@@ -121,6 +121,10 @@ static bool isDigit(char c) {
   return c >= '0' && c <= '9';
 }
 
+static bool isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
 static Token number() {
   while (isDigit(peek())) {
     advance();
@@ -138,6 +142,71 @@ static Token number() {
   return makeToken(TOKEN_NUMBER);
 }
 
+static TokenType checkKeyword(int start,
+    int length,
+    const char rest[length],
+    TokenType type) {
+  if (g_SCANNER.current - g_SCANNER.start == start + length
+      && memcmp(g_SCANNER.start + start, rest, length) == 0) {
+    return type;
+  }
+  return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+  switch (g_SCANNER.start[0]) {
+    case 'a':
+      return checkKeyword(1, 2, "nd", TOKEN_AND);
+    case 'c':
+      return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+    case 'e':
+      return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+    case 'f':
+      if (g_SCANNER.current - g_SCANNER.start > 1) {
+        switch (g_SCANNER.start[1]) {
+          case 'a':
+            return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+          case 'o':
+            return checkKeyword(2, 1, "r", TOKEN_FOR);
+          case 'u':
+            return checkKeyword(2, 1, "n", TOKEN_FUN);
+        }
+      }
+    case 'i':
+      return checkKeyword(1, 1, "f", TOKEN_IF);
+    case 'n':
+      return checkKeyword(1, 2, "il", TOKEN_NIL);
+    case 'o':
+      return checkKeyword(1, 1, "r", TOKEN_OR);
+    case 'p':
+      return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+    case 'r':
+      return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+    case 's':
+      return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+    case 't':
+      if (g_SCANNER.current - g_SCANNER.start > 1) {
+        switch (g_SCANNER.start[1]) {
+          case 'h':
+            return checkKeyword(2, 2, "is", TOKEN_THIS);
+          case 'r':
+            return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+        }
+      }
+    case 'v':
+      return checkKeyword(1, 2, "ar", TOKEN_VAR);
+    case 'w':
+      return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+  }
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek())) {
+    advance();
+  }
+  return makeToken(identifierType());
+}
+
 void initScanner(const char source[static 1]) {
   g_SCANNER.start = source;
   g_SCANNER.current = source;
@@ -153,6 +222,9 @@ Token scanToken(void) {
   }
 
   char c = advance();
+  if (isAlpha(c)) {
+    return identifier();
+  }
   if (isDigit(c)) {
     return number();
   }
